2737a3bca8bcb1217daf4d59d22969bb0c84ad4e
Avaruz <adhemar.sgv@gmail.com>
2013-01-16 19:00:33 -0400

    v 1.2.4
    New Extension Method

---
+++ Avaruz.FrameWork.Utils/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avaruz.FrameWork.Utils.Common;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var google2 = new Googlelizer();
+            Console.WriteLine(google2.GetMyGoogle("TALAVERA ANTELO OSCAR"));
+
+            Console.ReadLine();
+        }
+    }
+}
--- Avaruz.FrameWork.Utils/Utils.Common/MyExtensions.cs
+++ Avaruz.FrameWork.Utils/Utils.Common/MyExtensions.cs
@@ -9,28 +9,64 @@ namespace Avaruz.FrameWork.Utils.Common
             return !value1.Equals(value2) ? value2 : value1;
         }
 
+        public static string Googlelizer(this string cadenaOriginal)
+        {
+            var googlelizeString = "";
+            //Primero partir la cadena
+
+            googlelizeString =
+                cadenaOriginal.Replace("S", "[0]")
+                              .Replace("Z", "[1]")
+                              .Replace("C", "[2]")
+                              .Replace("G", "[3]")
+                              .Replace("Ñ", "[4]")
+                              .Replace("N", "[5]")
+                              .Replace("J", "[6]")
+                              .Replace("B", "[7]")
+                              .Replace("V", "[8]")
+                              .Replace("Y", "[9]")
+                              .Replace("I", "[10]");
+
+            googlelizeString =
+    googlelizeString.Replace("[0]", "[SCZ]")
+                  .Replace("[1]", "[SCZ]")
+                  .Replace("[2]", "[SCZ]")
+                  .Replace("[3]", "[GJ]")
+                  .Replace("[4]", "[NÑ]")
+                  .Replace("[5]", "[ÑN]")
+                  .Replace("[6]", "[GJ]")
+                  .Replace("[7]", "[BV]")
+                  .Replace("[8]", "[BV]")
+                  .Replace("[9]", "[YI]")
+                  .Replace("[10]", "[YI]");
+
+
+            return googlelizeString;
+        }
+
+
         public static DateTime ToDefaultDate(this string strfecha)
         {
-            DateTime fecha = strfecha == "" ? DateTime.Parse("01/01/0001") : DateTime.Parse(strfecha);
+            var fecha = strfecha == "" ? DateTime.Parse("01/01/0001") : DateTime.Parse(strfecha);
             return fecha;
         }
 
         public static DateTime? ToNullDate(this DateTime fechaOriginal)
         {
-            DateTime? fecha = fechaOriginal == DateTime.MinValue ? new DateTime?() : fechaOriginal;
+            var fecha = fechaOriginal == DateTime.MinValue ? new DateTime?() : fechaOriginal;
             return fecha;
         }
 
         public static string TrimIfNeed(this string cadena, int maxLenght)
         {
-            int cadenaLength = cadena.Length;
-            string nuevaCadena = cadenaLength > maxLenght ? cadena.Substring(0, maxLenght) : cadena.Trim();
+            var cadenaLength = cadena.Length;
+            var nuevaCadena = cadenaLength > maxLenght ? cadena.Substring(0, maxLenght) : cadena.Trim();
             return nuevaCadena;
         }
 
         public static string ToNoDefaultDate(this DateTime fecha)
         {
-            string strfecha = fecha.ToShortDateString() == "01/01/0001" ? "" : fecha.ToShortDateString();
+            var strfecha = fecha.ToShortDateString() == "01/01/0001" ? "" : fecha.ToShortDateString();
             return strfecha;
         }
 
